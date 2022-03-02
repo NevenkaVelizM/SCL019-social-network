1a96dd3e1fe8bfbdaf4a7c649cd3d4efda2d3d78
Anabella Lincopan <anabella.lincopan@ug.uchile.cl>
2022-03-02 11:42:38 -0400

    cambio en submit de index y se agrega archivo config

--- src/lib/registro.js
+++ src/lib/registro.js
@@ -1,64 +1,73 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
 import {
-getAuth,
-createUserWithEmailAndPassword,
-signInWithPopup, 
-GoogleAuthProvider} from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js';
-//signInWithEmailAndPassword
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  // signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
 
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-apiKey: "AIzaSyCilQ3aBq8YPJ8lllWOyFdWtUSNSWVevjg",
-authDomain: "social-network-codezan.firebaseapp.com",
-projectId: "social-network-codezan",
-storageBucket: "social-network-codezan.appspot.com",
-messagingSenderId: "946541643320",
-appId: "1:946541643320:web:871354dd18ba06271648cd",
-measurementId: "G-3MCLJ446BH"
-};
+import { firebaseConfig } from "./config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 
-//registro con email y constraseña
-export const registerUser= (userName, email, password) => {
-    createUserWithEmailAndPassword(auth, userName, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    console.log(user);
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode, errorMessage);
-    // ..
-  });
-}
+// registro con email y constraseña
+export const registerUser = (userName, email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      user.displayName = userName;
+      // console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      // ..
+    });
+};
 
 // login con google
-export const loginWithGoogle= () => {
-const provider = new GoogleAuthProvider();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
+export const loginWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
+
+// para conocer el estado de autenticación del usuario
+export const activeUser = () => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
   });
-}
+};
