b7bf7df9ffacabad6dd284b5314f47ed3562a792
Nevenka Véliz M <95089012+NevenkaVelizM@users.noreply.github.com>
2022-04-16 21:41:28 -0400

    Update index.spec.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,34 +1,8 @@
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-} from "../src/views/firebase-init";
+// importamos la funcion que vamos a testear
+import { myFunction } from '../src/lib/index';
 
-import { createUser } from "../src/views/firebase-auth.js";
-
-jest.mock("../src/views/firebase-init");
-
-describe("Auth", () => {
-  describe("createUser", () => {
-    test("should fail if called without arguments", () => {
-      expect(() => createUser()).toThrow();
-      expect(() => createUser("justEmail@test.com")).toThrow();
-    });
-    test("should call firebase createUserWithEmailAndPassword with auth object, email and password", async () => {
-      await createUser("justEmail@test.com", "password");
-      expect(createUserWithEmailAndPassword).toHaveBeenCalledWith(
-        getAuth(),
-        "justEmail@test.com",
-        "password",
-      );
-    });
-
-    test("should return createUserWithEmailAndPassword result", async () => {
-      const creationResult = await createUser("justEmail@test.com", "password");
-      expect(creationResult).toStrictEqual({
-        auth: getAuth(),
-        email: "justEmail@test.com",
-        password: "password",
-      });
-    });
+describe('myFunction', () => {
+  it('debería ser una función', () => {
+    expect(typeof myFunction).toBe('function');
   });
 });
