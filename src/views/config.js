873871e1cebae39da5fc91e7ea4dee18253eb5b0
Nevenka Véliz M <95089012+NevenkaVelizM@users.noreply.github.com>
2022-04-16 21:37:23 -0400

    Delete config.js

--- src/views/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCilQ3aBq8YPJ8lllWOyFdWtUSNSWVevjg",
-  authDomain: "social-network-codezan.firebaseapp.com",
-  projectId: "social-network-codezan",
-  storageBucket: "social-network-codezan.appspot.com",
-  messagingSenderId: "946541643320",
-  appId: "1:946541643320:web:871354dd18ba06271648cd",
-  measurementId: "G-3MCLJ446BH",
-};
